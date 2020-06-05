0647f535fb8564b4fcd2534154cb907a6bb05577
avrovla <66422390+avrovla@users.noreply.github.com>
2020-06-05 10:16:19 +0700

    init

---
+++ dkp/src/main/java/com/avrovla/dkp/DkpApplication.java
@@ -0,0 +1,13 @@
+package com.avrovla.dkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkpApplication.class, args);
+	}
+
+}
